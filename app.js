97264e04a096ea3ed4e6a8793b1bf5369233b0e0
tuy112 <master_catch@naver.com>
2023-07-25 21:54:53 +0900

    2023.07.25 (First Commit)

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(port, "포트가 열렸습니다~^^");
+});
